f6f86d3de042edffe43d94661c351fe2d1db7a81
m.sinans <msomercikoglu@gmail.com>
2023-03-21 12:01:10 +0300

    Add project files.

---
+++ Matematik_Fonksiyonlar/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matematik_Fonksiyonlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.ToString().Replace('.', ',');
+            double sayi = Convert.ToDouble(textBox1.Text);
+            //label2.Text = Math.Abs(sayi).ToString(); 
+            //label2.Text = Math.Ceiling(sayi).ToString();
+            label2.Text = Math.Floor(sayi).ToString();
+            //label2.Text = Math.Pow(sayi, 3).ToString();
+            //label2.Text = Math.Sqrt(sayi).ToString();
+            //label2.Text = Math.PI.ToString();
+
+            // Abs = Mutlak Değer,
+            // Ceiling = Yukarı Yuvarlar
+            // Floor = Aşağı Yuvarlar
+            // Pow = Üslü olarak çarpar (ikinci parametere üs olarak atanır)
+            // Sqrt = Karekök Alır
+
+        }
+    }
+}
